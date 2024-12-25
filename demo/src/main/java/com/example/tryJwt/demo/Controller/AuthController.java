a088fffc24d362c1f99d6911e00e03fd7f299189
Sebastián Butcovich <sebabutcovich@gmail.com>
2024-12-25 19:26:43 -0300

    Undecimo commit

--- demo/src/main/java/com/example/tryJwt/demo/Controller/AuthController.java
+++ demo/src/main/java/com/example/tryJwt/demo/Controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("auth")
