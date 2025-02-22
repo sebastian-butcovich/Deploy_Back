3d67398794553b33b2301b9884462bfa86406021
Sebastián Butcovich <sebabutcovich@gmail.com>
2025-02-21 22:52:56 -0300

    Se puede editar y eliminar un gasto con el front

--- demo/src/main/java/com/example/tryJwt/demo/Controller/AuthController.java
+++ demo/src/main/java/com/example/tryJwt/demo/Controller/AuthController.java
@@ -45,6 +45,7 @@ public class AuthController {
         return new String ("El servicio esta vivo");
     }
     @GetMapping("/validate")
+    @CrossOrigin(origins = "*")
     public Integer validate(@RequestParam String token)
     {
         return service.validate(token);
