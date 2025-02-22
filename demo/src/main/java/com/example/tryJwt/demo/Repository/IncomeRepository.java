31c1e88930ca0a925a8361efca6fac8dbe57f9b5
Sebastián Butcovich <sebabutcovich@gmail.com>
2025-02-22 08:37:09 -0300

    corrections

--- demo/src/main/java/com/example/tryJwt/demo/Repository/IncomeRepository.java
+++ demo/src/main/java/com/example/tryJwt/demo/Repository/IncomeRepository.java
@@ -10,13 +10,13 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface IncomeRepository extends JpaRepository<Income,Integer> {
-    @Query(value = "select * from income  where id_autor = ?1 ",nativeQuery = true)
+    @Query(value = "select * from income  where id_autor = ?1 order by id_autor desc",nativeQuery = true)
     public List<Income> findAllByUsuario(Integer id);
-    @Query(value = "select * from income where id_autor  = ?1 and monto between ?2 and ?3 ", nativeQuery = true)
+    @Query(value = "select * from income where id_autor  = ?1 and monto between ?2 and ?3 order by id_autor desc ", nativeQuery = true)
     public List<Income> findAllByUsuario(Integer id, Double monto_min, Double monto_max);
     //Busqueda con filtro tipo
-    @Query(value = "select * from income where id_autor  = ?1  and tipo = ?2", nativeQuery = true)
+    @Query(value = "select * from income where id_autor  = ?1  and tipo = ?2 order by id_autor desc", nativeQuery = true)
     public List<Income> findAllByUsuario(Integer id, String tipo);
-    @Query(value = "select * from income where id_autor  = ?1  and fecha between ?2 and ?3", nativeQuery = true)
+    @Query(value = "select * from income where id_autor  = ?1  and fecha between ?2 and ?3 order by id_autor desc", nativeQuery = true)
     public List<Income> findAllByUsuario(Integer id,  String fecha_inicio, String fecha_final);
 }
