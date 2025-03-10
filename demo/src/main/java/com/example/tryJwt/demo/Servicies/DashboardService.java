525655e280a3f495ffde6fd39e4202c6f90f2e33
Sebastián Butcovich <sebabutcovich@gmail.com>
2025-03-10 14:11:34 -0300

    Intento V6 de optimizar el dashboar

--- demo/src/main/java/com/example/tryJwt/demo/Servicies/DashboardService.java
+++ demo/src/main/java/com/example/tryJwt/demo/Servicies/DashboardService.java
@@ -130,7 +130,7 @@ public class DashboardService {
                         i++;
                     }
                     while(i<= spents.size()-1&&yearI == yearF && mesI<= spents.get(i).getFecha().getMonth()+1
-                            && spents.get(i).getFecha().getMonth()+1<mesF && diaI<= spents.get(i).getFecha().getDate()
+                            && spents.get(i).getFecha().getMonth()+1<=mesF && diaI<= spents.get(i).getFecha().getDate()
                             && spents.get(i).getFecha().getDate()>diaF)
                     {
                         suma+= spents.get(i).getMonto();
