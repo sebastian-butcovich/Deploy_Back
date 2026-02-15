1f076c0e34e7f35ebd2e09e35672ce3c14484ecd
bdgarat <garat.braian@gmail.com>
2026-02-14 21:22:51 -0300

    agregado docker compose para db y back

--- demo/target/generated-sources/annotations/com/example/tryJwt/demo/Mapper/ActualFlowMapperImpl.java
+++ demo/target/generated-sources/annotations/com/example/tryJwt/demo/Mapper/ActualFlowMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-02-14T20:43:07-0300",
+    date = "2026-02-14T20:45:20-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 25 (Oracle Corporation)"
 )
 @Component
--- demo/target/generated-sources/annotations/com/example/tryJwt/demo/Mapper/RegisterRequestMapperImpl.java
+++ demo/target/generated-sources/annotations/com/example/tryJwt/demo/Mapper/RegisterRequestMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-02-14T20:43:08-0300",
+    date = "2026-02-14T20:45:20-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 25 (Oracle Corporation)"
 )
 @Component
